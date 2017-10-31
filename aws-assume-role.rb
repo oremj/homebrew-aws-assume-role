22cafcc5e07974430d2aed7a9336d1e123196cdc
Jeremiah Orem <oremj@oremj.com>
2017-10-31 09:56:54 -0700

    Update aws-assume-role.rb

--- aws-assume-role.rb
+++ aws-assume-role.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class AssumeRole < Formula
+class AwsAssumeRole < Formula
   homepage 'https://github.com/oremj/aws-assume-role'
   url 'https://github.com/oremj/aws-assume-role/archive/v0.1.0.tar.gz'
   sha256 '5ecacbd2f8cc1becb069ad1bdd637068fec5481490d9342067e955a86170fc16'
